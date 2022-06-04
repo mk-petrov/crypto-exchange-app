e24f1b9d54196a9451d7ea01bbd5fe0a3af04857
Petko Petrov <petko.petrov89@gmail.com>
2022-06-04 18:15:15 +0300

    refactor api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,20 +3,25 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const binanceApi = {
   getPrice: async (pair: string):Promise<IAsset | null> => {
+    let price = 'The pair is not supported'
     try {
+      const [ firstCurrency = '', secondCurrency = '' ] = pair.split('/')
       const formatPair = pair.toLocaleUpperCase().replace('/', 'B')
       const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${formatPair}`);
       const result = await response.json();
 
-      return {
-        id: uuidv4(),
-        exchange: 'Binance',
-        price: result?.price
-      };
-    } catch (error) {
-      console.error(error)
-      return null
-    }
+
+      if (result?.price) {
+        price = `1 ${firstCurrency.toLocaleUpperCase()} = ${+result?.price} ${secondCurrency.toLocaleUpperCase()}`;
+      }
+      
+    } catch (error) {}
+
+    return {
+      id: uuidv4(),
+      exchange: 'Binance',
+      price,
+    };
   },
   // getDetails: async (pair: string):Promise => {
   //   try {

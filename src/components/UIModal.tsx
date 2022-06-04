051b3ec857a451b000e41e5143a084ba4dc55c5d
Petko Petrov <petko.petrov89@gmail.com>
2022-06-04 23:06:25 +0300

    add modal & loader

---
+++ src/components/UIModal.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+interface IProps {
+  isOpen: boolean;
+  onClose: (bool: boolean) => void;
+  children?: JSX.Element;
+}
+
+const UIModal: React.FC<IProps> = ({ isOpen, onClose, children }) => {
+  // const [open, setOpen] = React.useState(isOpen);
+  // const handleOpen = () => setOpen(true);
+  const handleClose = () => onClose(false);
+
+  return (
+    <div>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={isOpen}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={isOpen}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+
+export default UIModal;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as UITable } from './UITable';
 export { default as UISearch } from './UISearch';
+export { default as UILoader } from './UILoader';
+export { default as UIModal } from './UIModal';

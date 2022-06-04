1392b9ddd06faa8dbbcddbccf3f6634633ee6f0a
Petko Petrov <petko.petrov89@gmail.com>
2022-06-04 21:37:45 +0300

    add apis for different exchanges and implement the proxy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-// import { IAsset } from '../type';
+import { formatPrice } from './helpers';
 
 export const binanceApi = {
   getPrice: async (pair: string):Promise<IAsset | null> => {
@@ -7,19 +7,20 @@ export const binanceApi = {
     try {
       const [ firstCurrency = '', secondCurrency = '' ] = pair.split('/')
       const formatPair = pair.toLocaleUpperCase().replace('/', 'B')
-      const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${formatPair}`);
+      // const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${formatPair}`);
+      const response = await fetch(`/v3/ticker/price?symbol=${formatPair}`);
       const result = await response.json();
 
 
       if (result?.price) {
-        price = `1 ${firstCurrency.toLocaleUpperCase()} = ${+result?.price} ${secondCurrency.toLocaleUpperCase()}`;
+        price = `1 ${firstCurrency.toLocaleUpperCase()} = ${formatPrice(result.price, 2)} ${secondCurrency.toLocaleUpperCase()}`;
       }
       
     } catch (error) {}
 
     return {
       id: uuidv4(),
-      exchange: 'Binance',
+      exchange: 'BINANCE',
       price,
     };
   },
@@ -38,4 +39,83 @@ export const binanceApi = {
   //     return null
   //   }
   // }
+}
+
+export const bitfinexApi = {
+  getPrice: async (pair: string):Promise<IAsset | null> => {
+    let price = 'The pair is not supported'
+    try {
+      const [ firstCurrency = '', secondCurrency = '' ] = pair.split('/')
+      const formatPair = pair.toLocaleUpperCase().replace('/', '')
+      // const response = await fetch(`https://api-pub.bitfinex.com/v2/ticker/t${formatPair}`);
+      const response = await fetch(`/v2/ticker/t${formatPair}`);
+      const result = await response.json();
+      if (result && result.length > 6) {
+
+        price = `1 ${firstCurrency.toLocaleUpperCase()} = ${formatPrice(result[6], 2)} ${secondCurrency.toLocaleUpperCase()}`;
+      }
+      
+    } catch (error) {}
+
+    return {
+      id: uuidv4(),
+      exchange: 'BITFINEX',
+      price,
+    };
+  },
+}
+
+export const krakenApi = {
+  getPrice: async (pair: string):Promise<IAsset | null> => {
+    let price = 'The pair is not supported'
+    try {
+      const [ firstCurrency = '', secondCurrency = '' ] = pair.split('/')
+      const formatPair = pair.toLocaleUpperCase().replace('/', '')
+      // const response = await fetch(`https://api.kraken.com/0/public/Ticker?pair=${formatPair}`);
+      const response = await fetch(`/0/public/Ticker?pair=${formatPair}`);
+      const { result = {} } = await response.json();
+      if (Object.keys(result).length > 0) {
+        const currencyPairKey = Object.keys(result)[0];
+        const currentPrice = result[currencyPairKey].c[0]
+
+        if (currentPrice) {
+          price = `1 ${firstCurrency.toLocaleUpperCase()} = ${formatPrice(currentPrice, 2)} ${secondCurrency.toLocaleUpperCase()}`;
+        }
+      }
+      
+    } catch (error) {}
+
+    return {
+      id: uuidv4(),
+      exchange: 'KRAKEN',
+      price,
+    };
+  },
+}
+
+export const huobiApi = {
+  getPrice: async (pair: string):Promise<IAsset | null> => {
+    let price = 'The pair is not supported'
+    try {
+      const [ firstCurrency = '', secondCurrency = '' ] = pair.split('/')
+      const formatPair = pair.replace('/', '')
+      // const response = await fetch(`https://api.huobi.pro/market/history/kline?period=1min&size=1&symbol=${formatPair}`);
+      const response = await fetch(`/market/history/kline?symbol=${formatPair}&period=1min&size=1&`);
+      const { data = [] } = await response.json();
+      if (data.length > 0) {
+        const { close } = data[0];
+
+        if (close) {
+          price = `1 ${firstCurrency.toLocaleUpperCase()} = ${formatPrice(close, 2)} ${secondCurrency.toLocaleUpperCase()}`;
+        }
+      }
+      
+    } catch (error) {}
+
+    return {
+      id: uuidv4(),
+      exchange: 'HoubiGlobal',
+      price,
+    };
+  },
 }

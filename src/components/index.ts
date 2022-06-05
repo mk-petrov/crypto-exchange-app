9e0bb7d04a985bdd85ae8b123c8eb245b9f08ef0
Petko Petrov <petko.petrov89@gmail.com>
2022-06-05 11:47:30 +0300

    add UIButton

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as UITable } from './UITable';
 export { default as UISearch } from './UISearch';
 export { default as UILoader } from './UILoader';
 export { default as UIModal } from './UIModal';
+export { default as UIButton } from './UIButton';
